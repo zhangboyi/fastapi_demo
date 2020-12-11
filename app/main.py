4723171f74514db0886f9c457b331ac167868392
Boyi Zhang <boyi.zhang@shopee.com>
2020-12-11 18:59:08 +0800

    update main.py

--- app/main.py
+++ app/main.py
@@ -22,4 +22,4 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(host='0.0.0.0',port=8888)
+    uvicorn.run(app=app,host='0.0.0.0',port=8888)
